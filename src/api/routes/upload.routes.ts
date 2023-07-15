53c8878e4d40c5c0ee0eeaee153224d57d81a982
samuel santos <samueldev8@gmail.com>
2023-07-14 23:14:07 -0300

    feat: implement reqValidation

--- src/api/routes/upload.routes.ts
+++ src/api/routes/upload.routes.ts
@@ -3,9 +3,15 @@ import { type Router } from 'express';
 import { adaptRouter } from '../../adapters/express/route.adapter';
 import { makeProcessCsvController } from '../../factories';
 import { appendFile } from '../../middlewares';
+import { reqValidation } from '../../adapters/express/reqValidation.adapter';
+import { ProcessCsvValidation } from '../validations';
 
 export default (router: Router): void => {
   appendFile(router);
 
-  router.post('/csv', adaptRouter(makeProcessCsvController()));
+  router.post(
+    '/csv',
+    reqValidation(ProcessCsvValidation),
+    adaptRouter(makeProcessCsvController())
+  );
 };
---
+++ src/api/validations/processCsv.validation.ts
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+const fileNameRegex = /^[^!#$/%&*]+$/;
+const MAX_SIZE = 10 * 1024 * 1024;
+
+export const ProcessCsvValidation = {
+  file: Joi.object()
+    .keys({
+      buffer: Joi.binary().max(MAX_SIZE).required(),
+      mimetype: Joi.string().valid('text/csv').required(),
+      fileName: Joi.string().pattern(fileNameRegex).optional(),
+      size: Joi.number()
+        .messages({
+          'number.max': 'Size exceeds the maximum allowed value of 10MB'
+        })
+        .required()
+    })
+    .unknown(true)
+    .required()
+};
